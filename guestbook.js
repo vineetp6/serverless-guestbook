db1583191f0725c1b85ba32202c6227a1b3e428d
vineetp6 <vineetp6@gmail.com>
2022-07-04 20:22:43 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://3883bb8f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
